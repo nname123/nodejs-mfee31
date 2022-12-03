1cef54252891af83a61ba5b3aee712f966cfd06e
nname123 <zcdfh80040@gmail.com>
2022-12-03 15:57:57 +0800

    [feat]負數

--- sum.js
+++ sum.js
@@ -1,18 +1,26 @@
 function sum(n) {
     // 回傳 1+2+3+4+...+n 的結果
-    
+
     // if (n < 0) {
-    //     n = Math.abs(n)
+    //     n = -n
     //     result = (1 + n) * n / 2 * -1
     // } else {
     //     result = (1 + n) * n / 2
     // }
 
     result = 0
-    for (let i = 1; i <= n; i++) {
-        result += i
-
+    if (n < 0) {
+        n = -n
+        for (let i = 1; i <= n; i++) {
+            result += i
+        }
+        result = -result
+    } else {
+        for (let i = 1; i <= n; i++) {
+            result += i
+        }
     }
+
     return result
 }
 
