18c730cb9f0e85b2975ec7b6d01c8ee7e59377f1
nname123 <zcdfh80040@gmail.com>
2022-12-03 15:43:15 +0800

    [feat]還有for

--- sum.js
+++ sum.js
@@ -1,6 +1,18 @@
 function sum(n) {
     // 回傳 1+2+3+4+...+n 的結果
-    result=(1+n)*n/2
+    
+    // if (n < 0) {
+    //     n = Math.abs(n)
+    //     result = (1 + n) * n / 2 * -1
+    // } else {
+    //     result = (1 + n) * n / 2
+    // }
+
+    result = 0
+    for (let i = 1; i <= n; i++) {
+        result += i
+
+    }
     return result
 }
 
