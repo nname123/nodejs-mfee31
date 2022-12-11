2c98c5d79b7c3b97d1304d7d569906596f3e5221
nname123 <zcdfh80040@gmail.com>
2022-12-11 16:23:02 +0800

    [feat]爬蟲練習2.5

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,20 +1,11 @@
 const axios = require("axios");
-const fs = require("fs");
+const fs = require("fs/promises");
 
-let getStockNo = new Promise((resolve, reject) => {
-  // error-first callback
-  fs.readFile("stock.txt", "utf-8", (err, data) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(data);
-    }
-  });
-});
 
 (async () => {
   try {
-    let stockNo = await getStockNo;
+    let stockNo = await fs.readFile("stock.txt", "utf-8");
+    // console.log(stockNo);
     let date = "20221111";
     let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
       params: {
