93554c19f3c9ebbbc4b3c85bbd09dfc51585f9ad
nname123 <zcdfh80040@gmail.com>
2022-12-11 16:45:05 +0800

    [feat]加入時間moment

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,12 +1,13 @@
 const axios = require("axios");
 const fs = require("fs/promises");
-
+const moment = require("moment");
 
 (async () => {
   try {
     let stockNo = await fs.readFile("stock.txt", "utf-8");
     // console.log(stockNo);
-    let date = "20221111";
+    let date = moment().format("YYYYMMDD");
+    console.log(date);
     let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
       params: {
         stockNo,
