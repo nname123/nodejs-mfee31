86a3b67c4c2e33bba5ccfe01dc831bcb001c7f37
nname123 <zcdfh80040@gmail.com>
2022-12-17 12:05:26 +0800

    [feat]開始使用dotenv

--- crawler/test.js
+++ crawler/test.js
@@ -1,11 +1,12 @@
 const mysql2 = require("mysql2/promise");
+require("dotenv").config();
 (async () => {
   const connection = await mysql2.createConnection({
-    host: "localhost",
-    // port: 8080,
-    user: "admin",
-    password: "",
-    database: "stock_mfee31",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
   });
   try {
     // simple query
